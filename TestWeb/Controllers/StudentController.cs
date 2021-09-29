3dba21fdee03325649e8fe2397ac12bcb74f76fb
mqpasta <mqpasta@gmail.com>
2021-09-29 15:42:40 +0500

    Add project files.

---
+++ TestWeb/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestWeb.Models;
+
+namespace TestWeb.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Attendance(string id)
+        {
+            ViewBag.StudentName = id;
+            return View();
+        }
+
+        public IActionResult Create(Student s)
+        {
+            s.StudentName = "Yumna";
+            s.FatherName = "Zahid";
+            return View(s);
+        }
+
+        public IActionResult Save(Student s)
+        {
+
+            Student.StudentList.Add(s);
+
+            return View("List",Student.StudentList);
+        }
+    }
+}
---
+++ TestWeb/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWeb.Models
+{
+    public class Student
+    {
+        public static List<Student> StudentList = new List<Student>();
+
+        [Required]
+        [DisplayName("Name")]
+        public string StudentName { get; set; }
+        [Required]
+        [DisplayName("Father")]
+        public string FatherName { get; set; }
+        public decimal CGPA { get; set; }
+        public int Age { get; set; }
+
+    }
+}
